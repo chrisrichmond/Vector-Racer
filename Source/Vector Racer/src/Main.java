ad77a2cf959d97f69c31ee4da4ee002f16e386e0
Christopher Richmond CESM2015 <christopher.richmond.2015@uni.strath.ac.uk>
2018-10-25 15:03:56 +0100

    merge attempt

--- Source/Vector Racer/src/Main.java
+++ Source/Vector Racer/src/Main.java
@@ -1,5 +1,3 @@
-package sample;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -10,9 +8,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("View.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 

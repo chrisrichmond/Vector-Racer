93bb4f983d99c3ebfdc8d75425dc0b2ad6de9a5c
Christopher Richmond CESM2015 <christopher.richmond.2015@uni.strath.ac.uk>
2018-11-12 16:50:21 +0000

    Player next possible points

--- Source/Vector Racer/src/model/Player.java
+++ Source/Vector Racer/src/model/Player.java
@@ -18,8 +18,25 @@ public abstract class Player {
 
     public List<Point> getPossibleNextPoints(){
         List<Point> possibleNextPoints = new ArrayList<>();
+        int racerXpos = racer.getPosition().getX();
+        int racerYpos = racer.getPosition().getY();
+        int racerXvelo = racer.getVelocity().getXVelo();
+        int racerYvelo = racer.getVelocity().getYVelo();
 
-        possibleNextPoints.add(racer.getPosition()
+        /* get the central point out of the 9 possible next points
+            o o o
+            o x o
+            o o o
+         */
+
+        int centralX = racerXpos + racerXvelo;
+        int centralY = racerYpos + racerYvelo;
+
+        for(int y = (centralY-1); y <= (centralY+1); y++){
+            for(int x = (centralX-1); x <= (centralX+1); x++){
+                possibleNextPoints.add(new Point(x, y));
+            }
+        }
 
         return possibleNextPoints;
     }

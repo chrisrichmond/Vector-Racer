c52216478658e626941d1c9ff8bc6567c8b8eede
Christopher Richmond CESM2015 <christopher.richmond.2015@uni.strath.ac.uk>
2019-01-15 13:40:23 +0000

    fxml setup

--- Source/Vector Racer/src/view/Main.java
+++ Source/Vector Racer/src/view/Main.java
@@ -6,7 +6,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class View extends Application {
+public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);

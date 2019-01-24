077e01ceaade5d086419fb04d9bf385eaea66af8
Christopher Richmond CESM2015 <christopher.richmond.2015@uni.strath.ac.uk>
2019-01-24 21:26:46 +0000

    bump

--- Source/Vector Racer/src/controller/GameController.java
+++ Source/Vector Racer/src/controller/GameController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Region;
 import model.ModelAPI;
@@ -9,14 +10,18 @@ public class GameController implements Controller {
 
     private ModelAPI model;
     @FXML private Region tileGrid;
+    @FXML private Button testButton;
 
     public GameController(ModelAPI model){
         this.model = model;
     }
 
     public void setGridSize(){
-        tileGrid.setMinSize();
-        tileGrid.setMaxSize();
+        testButton.setMinSize(300,300);
+        testButton.setMaxSize(300,300);
+
+//        tileGrid.setMinSize();
+//        tileGrid.setMaxSize();
     }
 
 }

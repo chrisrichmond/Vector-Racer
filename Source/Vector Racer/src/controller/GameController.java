28222e9e24ec44efe8b0455031f5c673798325c4
Christopher Richmond CESM2015 <christopher.richmond.2015@uni.strath.ac.uk>
2019-02-08 06:42:39 +0000

    screenToModelCoord() method implemented for GameController to pass accurate MouseEvent coordinates back to Model in the future

--- Source/Vector Racer/src/controller/GameController.java
+++ Source/Vector Racer/src/controller/GameController.java
@@ -5,7 +5,11 @@ import javafx.event.EventHandler;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import model.ModelAPI;
+import utilities.VectorConstants;
 import view.ViewAPI;
+
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 
 public class GameController implements EventHandler {
 
@@ -27,5 +31,24 @@ public class GameController implements EventHandler {
         double y = ((MouseEvent)event).getY();
         System.out.println(x);
         System.out.println(y);
+
+        if(event instanceof MouseEvent){
+            double row = screenToModelCoord(y);
+            double col = screenToModelCoord(x);
+            System.out.println("SCREEN");
+            System.out.println("row "+y);
+            System.out.println("col "+x);
+            System.out.println("MODEL");
+            System.out.println("row "+row);
+            System.out.println("col "+col);
+        }
+    }
+
+    private double screenToModelCoord(double coord){
+        DecimalFormat df = new DecimalFormat("#.##");
+        df.setRoundingMode(RoundingMode.CEILING);
+        double twoDecimalPlaces = Double.parseDouble(df.format((coord / VectorConstants.TILESIZE)));
+
+        return Math.floor((twoDecimalPlaces +  (double)0.5 / 2) / 0.5) * 0.5;
     }
 }
